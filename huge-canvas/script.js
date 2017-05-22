34cbdfa7defebea6f3971c68d21627adcc0df72f
B-sirius <padfoot_07@163.com>
2017-05-22 09:40:38 +0800

    open img

--- huge-canvas/script.js
+++ huge-canvas/script.js
@@ -1,2 +1,34 @@
 'use strict';
-let canvas 
+
+var openImg = (function() {
+    var imgInput = document.getElementById('imgInput');
+
+    imgInput.onchange = function() {
+        openImg.call(this);
+    };
+
+    return function() {
+        var fileList = this.files;
+
+        var imgNode = document.createElement('img');
+        imgNode.src = window.URL.createObjectURL(fileList[0]);
+
+        imgNode.onload = function() {
+            console.log(Canvas);
+            Canvas.drawImage(this, 0, 0);
+        }
+    }
+})();
+
+var Canvas = (function() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    var drawImage = function(img, dx, dy) {
+        ctx.drawImage(img, dx, dy);
+    }
+
+    return {
+        drawImage: drawImage
+    };
+})();
