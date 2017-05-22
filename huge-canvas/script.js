e687cca538e900affd659b0864b283a2648382cf
B-sirius <padfoot_07@163.com>
2017-05-22 20:00:50 +0800

    thumbnail

--- huge-canvas/script.js
+++ huge-canvas/script.js
@@ -13,22 +13,103 @@ var openImg = (function() {
         var imgNode = document.createElement('img');
         imgNode.src = window.URL.createObjectURL(fileList[0]);
 
+        Mask.show();
+        Mask.setText('加载中...');
+
         imgNode.onload = function() {
-            console.log(Canvas);
+            Mask.hide();
+            Thumbnail.setImg(imgNode.src);
+            Thumbnail.setSelected();
             Canvas.drawImage(this, 0, 0);
         }
     }
 })();
 
-var Canvas = (function() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
+var Mask = (function() {
+    var mask = document.getElementById('mask');
+    var maskText = mask.getElementsByClassName('mask-text')[0];
 
-    var drawImage = function(img, dx, dy) {
-        ctx.drawImage(img, dx, dy);
+    var hide = function() {
+        mask.classList.add('hide');
+    }
+
+    var show = function() {
+        mask.classList.remove('hide');
+    }
+
+    var setText = function(text) {
+        maskText.textContent = text;
     }
 
     return {
-        drawImage: drawImage
+        hide: hide,
+        show: show,
+        setText: setText
+    }
+})();
+
+var Thumbnail = (function() {
+    var thumbnailImg = document.getElementById('thumbnail');
+    var selectedBox = document.getElementById('selected');
+
+    var setImg = function(src) {
+        thumbnailImg.src = src;
     };
+
+    var setSelected = function() {
+        var BenchmarkWidth = thumbnail.offsetWidth;
+        var naturalWidth = thumbnail.naturalWidth;
+        var canvas = Canvas.getCanvas();
+
+        var selectedBoxWidth = (BenchmarkWidth / naturalWidth * canvas.width).toFixed(2);
+        var selectedBoxHeight = (BenchmarkWidth / naturalWidth * canvas.height).toFixed(2);
+
+        selectedBox.style.width = selectedBoxWidth + 'px';
+        selectedBox.style.height = selectedBoxHeight + 'px';
+    };
+
+    return {
+        setImg: setImg,
+        setSelected: setSelected
+    };
+})();
+
+var Canvas = (function() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var canvasImg, offsetX, offSetY;
+
+    var drawImage = function(img, dx, dy) {
+        canvasImg = img;
+        offsetX = dx;
+        offSetY = dy;
+
+        _clearCanvas();
+        ctx.drawImage(img, dx, dy);
+    }
+
+    var scale = function(dx, dy) {
+        _clearCanvas();
+        ctx.scale(dx, dy);
+        ctx.drawImage(canvasImg, offsetX, offSetY);
+    }
+
+    var getCanvas = function() {
+        return canvas;
+    }
+
+    var _clearCanvas = function() {
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    return {
+        drawImage: drawImage,
+        scale: scale,
+        getCanvas: getCanvas
+    };
+})();
+
+var Controller = (function() {
+    
 })();
