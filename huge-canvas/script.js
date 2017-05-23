969c87f4f0e30eb7bf37925b15e52c53356f0bdb
B-sirius <padfoot_07@163.com>
2017-05-23 16:35:24 +0800

    drag complete

--- huge-canvas/script.js
+++ huge-canvas/script.js
@@ -18,8 +18,10 @@ var openImg = (function() {
 
         imgNode.onload = function() {
             Mask.hide();
+            Thumbnail.reset();
             Thumbnail.setImg(imgNode.src);
             Thumbnail.setSelected();
+            Canvas.reset();
             Canvas.setImage(this);
             Canvas.drawImage(0, 0);
         }
@@ -53,6 +55,10 @@ var Thumbnail = (function() {
     var thumbnailImg = document.getElementById('thumbnail');
     var selectedBox = document.getElementById('selected');
 
+    var ratio;
+    var offsetX = 0,
+        offsetY = 0;
+
     var setImg = function(src) {
         thumbnailImg.src = src;
     };
@@ -62,16 +68,45 @@ var Thumbnail = (function() {
         var naturalWidth = thumbnail.naturalWidth;
         var canvas = Canvas.getCanvas();
 
-        var selectedBoxWidth = (BenchmarkWidth / naturalWidth * canvas.width).toFixed(2);
-        var selectedBoxHeight = (BenchmarkWidth / naturalWidth * canvas.height).toFixed(2);
+        ratio = BenchmarkWidth / naturalWidth;
+
+        var selectedBoxWidth = (ratio * canvas.width).toFixed(2);
+        var selectedBoxHeight = (ratio * canvas.height).toFixed(2);
 
         selectedBox.style.width = selectedBoxWidth + 'px';
         selectedBox.style.height = selectedBoxHeight + 'px';
+
+        setPos(0, 0);
     };
+
+    var setPos = function(dx, dy) {
+        var x = limit(-dx * ratio + offsetX, 0, thumbnailImg.offsetWidth - selectedBox.offsetWidth);
+        var y = limit(-dy * ratio + offsetY, 0, thumbnailImg.offsetHeight - selectedBox.offsetHeight);
+
+        selectedBox.style.left = x + 'px';
+        selectedBox.style.top = y + 'px';
+    }
+
+    var setOffset = function(dx, dy) {
+        offsetX += -dx * ratio;
+        offsetY += -dy * ratio;
+
+        offsetX = limit(offsetX, 0, thumbnailImg.offsetWidth - selectedBox.offsetWidth);
+        offsetY = limit(offsetY, 0, thumbnailImg.offsetHeight - selectedBox.offsetHeight);
+
+    }
+
+    var reset = function() {
+        offsetX = 0;
+        offsetY = 0;
+    }
 
     return {
         setImg: setImg,
-        setSelected: setSelected
+        setSelected: setSelected,
+        setPos: setPos,
+        setOffset: setOffset,
+        reset: reset
     };
 })();
 
@@ -97,7 +132,7 @@ var Canvas = (function() {
             startY = e.offsetY;
 
             canvas.addEventListener('mousemove', mouseMove);
-            document.body.addEventListener('mouseup', rightMouseUp);
+            document.body.addEventListener('mouseup', mouseUp);
         }
     }, false);
 
@@ -106,13 +141,20 @@ var Canvas = (function() {
         throttleMoveCanvas(e, startX, startY);
     }
 
-    var rightMouseUp = function(e) {
+    var mouseUp = function(e) {
         if (e.button === 2) {
-            offsetX += e.offsetX - startX;
-            offsetY += e.offsetY - startY;
+            mediator.setOffset(e, startX, startY);
 
             canvas.removeEventListener('mousemove', mouseMove);
         }
+    }
+
+    var setOffset = function(dx, dy) {
+        offsetX += dx;
+        offsetY += dy;
+
+        offsetX = -limit(-offsetX, 0, canvasImg.naturalWidth - canvas.width);
+        offsetY = -limit(-offsetY, 0, canvasImg.naturalHeight - canvas.height);
     }
 
     var setImage = function(img) {
@@ -121,7 +163,13 @@ var Canvas = (function() {
 
     var drawImage = function(dx, dy) {
         _clearCanvas();
-        ctx.drawImage(canvasImg, offsetX + dx, offsetY + dy);
+
+        var x, y;
+
+        x = -limit(-(offsetX + dx), 0, canvasImg.naturalWidth - canvas.width);
+        y = -limit(-(offsetY + dy), 0, canvasImg.naturalHeight - canvas.height);
+
+        ctx.drawImage(canvasImg, x, y);
     }
 
     var scale = function(dx, dy) {
@@ -134,6 +182,11 @@ var Canvas = (function() {
         return canvas;
     }
 
+    var reset = function() {
+        offsetX = 0;
+        offsetY = 0;
+    }
+
     var _clearCanvas = function() {
         ctx.setTransform(1, 0, 0, 1, 0, 0);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -143,28 +196,11 @@ var Canvas = (function() {
         setImage: setImage,
         drawImage: drawImage,
         scale: scale,
-        getCanvas: getCanvas
+        getCanvas: getCanvas,
+        reset: reset,
+        setOffset: setOffset
     };
 })();
-
-// 节流函数工厂
-var throttle = function(fn, delay) {
-    var timer = null;
-    return function() {
-        if (timer) {
-            return false;
-        }
-        var self = this;
-        var args = arguments;
-
-        timer = setTimeout(function() {
-            clearTimeout(timer);
-            timer = null;
-
-            fn.apply(self, args);
-        }, delay);
-    }
-}
 
 // 中介者
 var mediator = (function() {
@@ -180,9 +216,62 @@ var mediator = (function() {
             dy = e.offsetY - startY;
 
         Canvas.drawImage(dx, dy);
+        Thumbnail.setPos(dx, dy);
+    }
+
+    var setOffset = function() {
+        var args = [].slice.apply(arguments);
+
+        var e = args.shift();
+
+        var startX = args[0],
+            startY = args[1];
+
+        var dx = e.offsetX - startX,
+            dy = e.offsetY - startY;
+
+        Canvas.setOffset(dx, dy);
+        Thumbnail.setOffset(dx, dy);
     }
 
     return {
-        moveCanvas: moveCanvas
+        moveCanvas: moveCanvas,
+        setOffset: setOffset
     }
 })();
+
+// 节流函数工厂
+var throttle = function(fn, delay) {
+    var timer;
+    var firstTime = true;
+
+    return function() {
+        var self = this;
+        var args = arguments;
+
+        if (firstTime) {
+            fn.apply(self, arguments);
+            return firstTime = false;
+        }
+
+        if (timer) {
+            return false;
+        }
+
+        timer = setTimeout(function() {
+            clearTimeout(timer);
+            timer = null;
+            fn.apply(self, args);
+        }, delay || 100);
+    }
+}
+
+var limit = function(val, bottom, top) {
+    if (val < bottom) {
+        val = bottom;
+    } else if (val > top) {
+        val = top;
+    }
+
+    return val;
+}
